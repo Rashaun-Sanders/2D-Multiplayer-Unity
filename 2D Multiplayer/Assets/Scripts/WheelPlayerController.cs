9c61982f37e84a297b7f589f494a12300ef0e645
ArkhamRider468 <W209458400@student.hccs.edu>
2021-04-19 00:02:54 -0500

    1

---
+++ 2D Multiplayer/Assets/Scripts/WheelPlayerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WheelPlayerController : MonoBehaviour
+{
+    public float movementSpeed;
+    public float jumpForce;
+
+    private Rigidbody2D _rigidbody;
+
+
+    private void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        var movemet = Input.GetAxis("Horizontal");
+        transform.position += new Vector3(movemet, 0, 0) * Time.deltaTime * movementSpeed;
+
+        if(Input.GetButtonDown("Jump") && Mathf.Abs(_rigidbody.velocity.y) < 0.001f)
+        {
+            _rigidbody.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
+        }
+    }
+}
